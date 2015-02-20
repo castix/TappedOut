7f1e8456a34d05e90ed1081582fcfe8cee798e38
castix <castix@users.noreply.github.com>
2015-02-20 08:56:54 +0100

    Update TappedOutSettings.mm

--- tappedoutsettings/TappedOutSettings.mm
+++ tappedoutsettings/TappedOutSettings.mm
@@ -1,1 +1,32 @@
-Placeholder
+#import <Preferences/Preferences.h>
+
+@interface TappedOutSettingsListController: PSListController {
+}
+@end
+
+@implementation TappedOutSettingsListController
+- (id)specifiers {
+	if(_specifiers == nil) {
+		_specifiers = [[self loadSpecifiersFromPlistName:@"TappedOutSettings" target:self] retain];
+	}
+	return _specifiers;
+}
+- (void)contact { 
+	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"mailto:development.castix@gmail.com"]]; 
+}
+@end
+
+@interface MITLicenseListController: PSListController {
+}
+@end
+
+@implementation MITLicenseListController
+- (id)specifiers {
+	if(_specifiers == nil) {
+		_specifiers = [[self loadSpecifiersFromPlistName:@"MITLicense" target:self] retain];
+	}
+	return _specifiers;
+}
+@end
+// vim:ft=objc
+
